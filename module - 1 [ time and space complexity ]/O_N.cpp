fe78f92eb8d337de0bb7f2f5a4181cb9197b1320
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-28 22:02:13 +0600

    Time Complexity of O(N)

---
+++ module - 1 [ time and space complexity ]/O_N.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int n, sum = 0;
+      cin >> n;
+
+      int a[n];
+
+      for (int i = 0; i < n; i++) // O(N)
+            cin >> a[i];
+
+      for (int i = 0; i < n; i++) // O{N}
+            sum += a[i];
+
+      cout << sum;
+
+      return 0;
+}
