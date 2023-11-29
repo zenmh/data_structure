38d02c4229bd502608b2003ff7ce49b4be7635c1
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-29 21:06:42 +0600

    Vector Insert

---
+++ module -2 [ stl vector ]/vector_insert.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      /*  insert-- > Insert elements at a specific position-- > O(N + K);
+       where K is the number of elements to be inserted
+
+       Here -->
+       N = hoyto last index projonto jaowa lagte pare for worst case tai N nite hobe
+       K = koyta value inset korte hobe seta bujay
+       EX. N er value jodi 5 hoy r jodi 100 ta value insert korte hoy tobe time complexity hobe O(N + K) and k er man besi howya e complexity hobe O(K) */
+
+      vector<int> v1 = {1, 2, 3, 4, 5}, v2 = {10, 20, 30, 40, 50};
+      // insert a value
+      v1.insert(v1.begin() + 3, 111);
+      for (int x : v1)
+            cout << x << " ";
+
+      // insert a vector or multiple value
+      v1.insert(v1.begin() + 3, v2.begin(), v2.end());
+      for (int x : v1)
+            cout << x << " ";
+
+      return 0;
+}
