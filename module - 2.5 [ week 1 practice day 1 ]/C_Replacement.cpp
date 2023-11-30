1dc27c9230101f2ccf72aaf1e14b0805508c748a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-30 20:39:33 +0600

    C - Replacement [ using vector ]

---
+++ module - 2.5 [ week 1 practice day 1 ]/C_Replacement.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int n;
+      cin >> n;
+
+      vector<int> v(n);
+
+      for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+      for (int i = 0; i < n; i++)
+            if (v[i] > 0)
+                  v[i] = 1;
+            else if (v[i] < 0)
+                  v[i] = 2;
+
+      for (int x : v)
+            cout << x << " ";
+
+      return 0;
+}
