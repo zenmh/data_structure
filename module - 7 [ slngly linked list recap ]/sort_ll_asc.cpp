c4080b34a52df5ad2295657ca659cb6c61f0b551
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-08 03:04:27 +0600

    Sort a linked list on ascending order using selection sort

---
+++ module - 7 [ slngly linked list recap ]/sort_ll_asc.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+      int val;
+      Node *next;
+
+      Node(int val)
+      {
+            this->val = val;
+            this->next = NULL;
+      }
+};
+
+void insert_at_tail_with_track(Node *&head, Node *&tail, int value) // O(1)
+{
+      Node *new_node = new Node(value);
+
+      if (head == NULL)
+      {
+            head = new_node;
+            tail = new_node;
+      }
+      else
+      {
+            tail->next = new_node;
+            tail = new_node;
+      }
+}
+
+void print_the_linked_list(Node *head) // O(N)
+{
+      Node *tmp = head;
+
+      while (tmp)
+      {
+            cout << tmp->val << " ";
+            tmp = tmp->next;
+      }
+
+      cout << endl;
+}
+
+int main()
+{
+
+      int x;
+      Node *head = NULL, *tail = NULL;
+
+      while (1) // O(N)
+      {
+            cin >> x;
+
+            if (x == -1)
+                  break;
+            else
+                  insert_at_tail_with_track(head, tail, x);
+      }
+
+      for (Node *i = head; i->next != NULL; i = i->next) // O(N*N) --> it work for max 1000 values
+            for (Node *j = i->next; j != NULL; j = j->next)
+                  if (i->val > j->val)
+                        swap(i->val, j->val);
+
+      print_the_linked_list(head);
+
+      return 0;
+}
