a50f1c62606ded2198f8d44e1f70d051d5b7232a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-10 22:59:25 +0600

    Some common constructor for list

--- module - 10 [ stl list and cycle detection ]/list_constructor.cpp
+++ module - 10 [ stl list and cycle detection ]/list_constructor.cpp
@@ -6,10 +6,21 @@ int main()
       list<int> ml1; // O(1)
       cout << ml1.size() << endl;
 
-      list<int> ml2(5, 100); // O(N)
+      list<int> ml2(10); // O(N) --> It makes 10 node with garbase value
 
-      for (auto it = ml2.begin(); it != ml2.end(); it++) // it++ goes to next node internally
+      list<int> ml3(5, 100); // O(N) --> It makes 10 node with value 100
+
+      for (auto it = ml3.begin(); it != ml3.end(); it++) // it++ goes to next node internally
             cout << *it << " ";
+
+      list<int> ml4(ml3); // O(N)
+      for(int val : ml4) cout << val << " ";
+
+      int n = 5, a[n] = {10, 20, 30, 40, 50};
+      list<int> ml5(a, a + n);
+
+      vector<int> v = {100, 200, 300, 400, 500};
+      list<int> ml6(v.begin(), v.end());
 
       return 0;
 }
