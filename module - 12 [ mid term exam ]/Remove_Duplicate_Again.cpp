a11db2f2cc985e0bee584d446c396336f31d0207
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-14 20:30:35 +0600

    Remove Duplicate Again - [ Mid term ]

---
+++ module - 12 [ mid term exam ]/Remove_Duplicate_Again.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int v;
+    list<int> ml;
+
+    while(1)
+    {
+        cin >> v;
+
+        if (v == -1) break;
+
+        ml.push_back(v);
+    }
+
+    ml.sort();
+    ml.unique();
+
+    for (int x : ml)
+    cout << x << " ";
+
+    return 0;
+}
