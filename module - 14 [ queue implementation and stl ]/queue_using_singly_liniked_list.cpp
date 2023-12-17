26933568753497204ccf1f710250ffae7b413586
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-18 00:20:57 +0600

    Queue using singly linked list

---
+++ module - 14 [ queue implementation and stl ]/queue_using_singly_liniked_list.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+      int val;
+      Node *next;
+
+      Node(int val)
+      {
+            this->val = val;
+            this->next = NULL;
+      }
+};
+
+class MyQueue
+{
+public:
+      Node *head = NULL, *tail = NULL;
+      int sz = 0;
+
+      void push(int val)
+      {
+            Node *new_node = new Node(val);
+
+            if (!head)
+            {
+                  head = new_node;
+                  tail = new_node;
+            }
+            else
+            {
+                  tail->next = new_node;
+                  tail = new_node;
+            }
+
+            sz++;
+      }
+      void pop()
+      {
+            Node *del_node = head;
+            head = head->next;
+
+            if (!head)
+            {
+                  tail = NULL;
+            }
+
+            sz--;
+      }
+      int front()
+      {
+            return head->val;
+      }
+      int size()
+      {
+            return sz;
+      }
+      bool empty()
+      {
+            if (sz == 0)
+                  return true;
+            else
+                  return false;
+      }
+};
+
+int main()
+{
+      MyQueue mq;
+      int n, x;
+      cin >> n;
+
+      for (int i = 0; i < n; i++)
+      {
+            cin >> x;
+            mq.push(x);
+      }
+
+      while (!mq.empty())
+      {
+            cout << mq.front() << endl;
+            mq.pop();
+      }
+
+      return 0;
+}
