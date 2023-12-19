9c1650c206e6ab4fffcc0caf8ab83d737869fbef
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-19 23:37:38 +0600

    Reversing a Queue [ Coding Ninjas ]

---
+++ module - 15 [ problem solving using stack & queue ]/Reversing_a_Queue.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+queue<int> reverseQueue(queue<int> q)
+{
+      stack<int> s;
+
+      while (!q.empty())
+      {
+            s.push(q.front());
+            q.pop();
+      }
+
+      while (!s.empty())
+      {
+            q.push(s.top());
+            s.pop();
+      }
+
+      return q;
+}
