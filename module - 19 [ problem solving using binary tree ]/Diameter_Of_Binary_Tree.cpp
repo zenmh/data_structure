82b1f59d5cc3ad9a39c286273934eb93e61499f1
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-04 21:26:32 +0600

    Diameter Of Binary Tree - [ Coding Ninjas ]

---
+++ module - 19 [ problem solving using binary tree ]/Diameter_Of_Binary_Tree.cpp
@@ -0,0 +1,29 @@
+
+int mx = 0;
+
+int maxHeight(TreeNode<int> *root)
+{
+      if (!root)
+            return 0;
+
+      int l = maxHeight(root->left);
+      int r = maxHeight(root->right);
+
+      int d = l + r;
+
+      mx = max(mx, d);
+
+      return max(l, r) + 1;
+}
+
+int diameterOfBinaryTree(TreeNode<int> *root)
+{
+      mx = 0;
+
+      int l = maxHeight(root->left);
+      int r = maxHeight(root->right);
+
+      int d = l + r;
+
+      return max(mx, d);
+}
