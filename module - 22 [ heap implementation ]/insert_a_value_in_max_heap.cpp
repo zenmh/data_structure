97630dfee05e354a09565ac3e9bed555de3488b3
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-09 21:03:12 +0600

    Insert a value in max heap

---
+++ module - 22 [ heap implementation ]/insert_a_value_in_max_heap.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      vector<int> v = {50, 40, 45, 30, 35, 20, 10, 11, 12, 15};
+
+      int x;
+
+      cin >> x;
+
+      v.push_back(x);
+
+      int cur_idx = v.size() - 1;
+
+      while (cur_idx != 0)
+      {
+            int parent_idx = (cur_idx - 1) / 2;
+            if (v[parent_idx] < v[cur_idx])
+                  swap(v[parent_idx], v[cur_idx]);
+            else
+                  break;
+
+            cur_idx = parent_idx;
+      }
+
+      for (int x : v)
+            cout << x << ' ';
+
+      return 0;
+}
