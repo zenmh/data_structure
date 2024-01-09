a9ebfdea68bb312347ea233fd75fb01154039d58
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-09 21:09:33 +0600

    Insert N values in max heap

---
+++ module - 22 [ heap implementation ]/insert_n_valuse_in_max_heap.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      vector<int> v;
+
+      int n, x;
+
+      cin >> n;
+
+      while (n--) // O(NlogN)
+      {
+            cin >> x;
+
+            v.push_back(x);
+
+            int cur_idx = v.size() - 1;
+
+            while (cur_idx != 0) // O(logN)
+            {
+                  int parent_idx = (cur_idx - 1) / 2;
+                  if (v[parent_idx] < v[cur_idx])
+                        swap(v[parent_idx], v[cur_idx]);
+                  else
+                        break;
+
+                  cur_idx = parent_idx;
+            }
+      }
+
+      for (int x : v)
+            cout << x << ' ';
+
+      return 0;
+}
