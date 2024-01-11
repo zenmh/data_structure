89861adbb2e8d3bb5d01c2831de8addd7c62e84b
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-11 22:18:22 +0600

    Check is the value exist by the key

--- module - 23 [ stl priority queue, map and set ]/stl_map.cpp
+++ module - 23 [ stl priority queue, map and set ]/stl_map.cpp
@@ -5,12 +5,17 @@ int main()
 {
       map<string, int> m;
 
-      m["abir"] = 66;
+      m["abir"] = 66; // O(logN)
       m["evan"] = 79;
       m["chan"] = 59;
 
       for (auto it = m.begin(); it != m.end(); it++)
-            cout << it->first << ' ' << it->second << endl;
+            cout << it->first << ' ' << it->second << endl; // O(logN)
+
+      if (m["Abir"])
+            cout << "YES";
+      else
+            cout << "NO";
 
       return 0;
 }
