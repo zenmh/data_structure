b594bc7ff7b001bb26160156c6f37c433de2da37
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-11 16:04:49 +0600

    Min hepa using priority queue

---
+++ module - 23 [ stl priority queue, map and set ]/min_heap_using_priority_queue.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      priority_queue<int> pq; // O{logN}
+
+      while (1) // O(NlogN)
+      {
+            int x, v;
+
+            cin >> x;
+
+            if (x == 0)
+                  pq.pop();
+            else if (x == 1)
+            {
+                  cin >> v;
+                  pq.push(v);
+            }
+            else if (x == 2)
+                  cout << pq.top() << endl;
+            else
+                  break;
+      }
+
+      return 0;
+}
